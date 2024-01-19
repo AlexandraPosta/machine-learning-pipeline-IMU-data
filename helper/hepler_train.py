05aa7da4848c0ce8ce8596cbbedc96189e3e8cc9
Alexandra Posta <posta_alex222@yahoo.com>
2024-01-19 20:35:29 +0000

    feat(train): include initial models for ANN and SVM

--- helper/hepler_train.py
+++ helper/hepler_train.py
@@ -3,14 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 # ANN model
-# Define the neural network model
-class SimpleNet(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(SimpleNet, self).__init__()
-        self.linear = nn.Linear(input_size, output_size)
 
-    def forward(self, x):
-        return self.linear(x)
 
 # SVM model
 
